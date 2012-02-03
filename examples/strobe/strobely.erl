df8691c6224322356e32187c781494e1456b2731
Michael Santos <michael.santos@gmail.com>
2012-02-03 17:28:29 -0500

    Example of using srly

--- examples/strobe/strobely.erl
+++ examples/strobe/strobely.erl
@@ -28,7 +28,10 @@
 %% LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
 %% ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %% POSSIBILITY OF SUCH DAMAGE.
--module(strobe).
+
+%% Example using srly
+
+-module(strobely).
 -export([start/0, start/1]).
 
 -define(LEDS, 5).
@@ -44,26 +47,11 @@ start(Opt) ->
     Leds = proplists:get_value(leds, Opt, ?LEDS),
     Interval = proplists:get_value(interval, Opt, ?INTERVAL),
 
-    {ok,FD} = serctl:open(Dev),
-
-    Termios = lists:foldl(
-        fun(Fun, Acc) -> Fun(Acc) end,
-        serctl:mode(raw),
-        [
-            fun(N) -> serctl:flow(N, false) end,
-            fun(N) -> serctl:ispeed(N, b9600) end,
-            fun(N) -> serctl:ospeed(N, b9600) end
-        ]
-    ),
-
-    ok = serctl:tcsetattr(FD, tcsanow, Termios),
-
-    {ok, Termios1} = serctl:tcgetattr(FD),
-    false = serctl:flow(Termios1),
+    {ok, FD} = srly:open(Dev, [{flow, false}, {speed, b9600}]),
 
     blink(FD, Interval, lists:seq(0,Leds)).
 
 blink(FD, N, [H|T]) ->
-    ok = serctl:write(FD, <<(1 bsl H):4/big-unsigned-integer-unit:8>>),
+    ok = srly:send(FD, <<(1 bsl H):4/big-unsigned-integer-unit:8>>),
     timer:sleep(N),
     blink(FD, N, T ++ [H]).
