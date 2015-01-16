4ad871deef0f2b0de7d829c07c4e5290d941084a
Michael Santos <michael.santos@gmail.com>
2015-01-16 14:02:04 -0500

    Add hw tests for non-destructive functions
    Test requiring hardware. To use the environment variable must be set:
    
        export SRLY_TEST_PORT=/dev/ttyUSB0

--- test/serctl_tests.erl
+++ test/serctl_tests.erl
@@ -5,5 +5,72 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("srly/include/serctl.hrl").
 
+-record(state, {
+        fd,
+        termios
+    }).
+
 constant_test() ->
     [ V = serctl:constant(K) || {K,V} <- serctl:constant() ].
+
+serctl_test_() ->
+    case os:getenv("SRLY_TEST_PORT") of
+        false ->
+            [];
+        _ ->
+            {setup,
+                fun start/0,
+                fun stop/1,
+                fun run/1}
+    end.
+
+start() ->
+    Dev = os:getenv("SRLY_TEST_PORT"),
+    {ok, FD} = serctl:open(Dev),
+    {ok, Termios} = serctl:tcgetattr(FD),
+    #state{
+        fd = FD,
+        termios = serctl:termios(Termios)
+    }.
+
+stop(#state{fd = FD}) ->
+    serctl:close(FD).
+
+run(State) ->
+    [
+        getfd(State),
+        flow(State),
+        mode(State),
+        ispeed(State),
+        ospeed(State),
+        baud(State)
+    ].
+
+getfd(#state{fd = FD}) ->
+    N = serctl:getfd(FD),
+    ?_assertEqual(true, is_integer(N)).
+
+flow(#state{termios = Termios}) ->
+    N = serctl:flow(Termios),
+    ?_assertEqual(true, N == true orelse N == false).
+
+mode(#state{}) ->
+    N = serctl:mode(raw),
+    ?_assertMatch(#termios{}, N).
+
+ispeed(#state{termios = Termios}) ->
+    N = serctl:ispeed(Termios),
+    ?_assertEqual(true, is_integer(N)).
+
+ospeed(#state{termios = Termios}) ->
+    N = serctl:ospeed(Termios),
+    ?_assertEqual(true, is_integer(N)).
+
+baud(#state{}) ->
+    N = serctl:baud(115200),
+    case os:type() of
+        {unix,linux} ->
+            ?_assertMatch(4098, N);
+        _ ->
+            []
+    end.
